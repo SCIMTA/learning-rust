c6013d4724a6d03dc5ef118a560aced5bd2294a5
Thân Trọng Thành <thanhtt@aiacademy.edu.vn>
2022-06-29 20:00:19 +0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-mod buoi1;
-mod buoi2;
-mod buoi3;
-mod buoi4;
+// Bài 2: Lifetime
+// Yêu cầu: Sửa lỗi Lifetime 
+
+use std::fmt;
+struct StrDisplayable<'a>(Vec<&'a str>);
+
+impl<'a> fmt::Display for StrDisplayable<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for v in &self.0 {
+            write!(f, "\n{}", v)?;
+        }
+        Ok(())
+    }
+    
+}
+
 fn main() {
-    buoi4::bai2();
+    let vec: Vec<&str> = vec!["a", "bc", "def"];
+    let vec_Foo = StrDisplayable(vec);
+    println!("{}", vec_Foo);
 }
